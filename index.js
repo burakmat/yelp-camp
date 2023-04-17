5bc39eae6b417929d79a37e14178bdf6d3f804cd
Burak Mat <burakk.mat@gmail.com>
2023-04-17 23:52:40 +0300

    add CRUD operations

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express()
 const path = require("path")
 const mongoose = require("mongoose")
 const Campground = require("./models/campground")
+const methodOverride = require("method-override")
 
 
 mongoose.connect("mongodb://localhost:27017/yelp-camp")
@@ -20,16 +21,44 @@ app.listen(3000, () => {
 	console.log("Listening on port 3000.")
 })
 
+app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride("_method"))
+
 app.get("/", (req, res) => {
 	res.render("home")
 })
 
-app.get("/make", async (req, res) => {
-	const camp = new Campground({
-		title: "My Backyard",
-		description: "cheap camping"
-	})
-	await camp.save()
-	res.send(camp)
+app.get("/campgrounds", async (req, res) => {
+	const cg = await Campground.find({});
+	res.render("campgrounds/index", { campgrounds: cg })
 })
 
+app.get("/campgrounds/new", (req, res) => {
+	res.render("campgrounds/new")
+})
+
+app.post("/campgrounds", async (req, res) => {
+	const campground = new Campground(req.body.campground)
+	await campground.save()
+	res.redirect(`/campgrounds/${campground._id}`)
+})
+
+app.get("/campgrounds/:id", async (req, res) => {
+	const campground = await Campground.findById(req.params.id);
+	res.render("campgrounds/show", { campground })
+})
+
+app.get("/campgrounds/:id/edit", async (req, res) => {
+	const campground = await Campground.findById(req.params.id);
+	res.render("campgrounds/edit", { campground })
+})
+
+app.put("/campgrounds/:id", async (req, res) => {
+	const campground = await Campground.findByIdAndUpdate(req.params.id, { ...req.body.campground })
+	res.redirect(`/campgrounds/${campground._id}`)
+})
+
+app.delete("/campgrounds/:id", async (req, res) => {
+	await Campground.findByIdAndDelete(req.params.id);
+	res.redirect("/campgrounds")
+})
